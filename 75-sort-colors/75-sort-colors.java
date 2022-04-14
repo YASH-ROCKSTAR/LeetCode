77c81e67f96cb34c657cd33e45d3fe5daed1dbfa
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-04-14 11:34:47 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (81.48%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,56 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-    //     int low=0;
-    //     int mid=0;
-    //     int high=nums.length-1;
-    //     while(mid<=high){
-    //         if(nums[mid]==0){
-    //             swap(nums,low,mid);
-    //             mid++;
-    //             low++;
-    //         }
-    //         else if(nums[mid]==1){
-    //             mid++;
-    //         }
-    //         else{
-    //             swap(nums,high,mid);
-    //             high--;
-    //             mid++;
-    //         }
-    //     }
-    // }
-    // public void swap(int[] arr,int i,int j){
-    //     int temp=arr[i];
-    //     arr[i]=arr[j];
-    //     arr[j]=temp;
-    // }
-        int i=0, j=0, k=nums.length-1;
-        
-		//i-> 0's pointer, j-> 1's pointer, k->2's pointer
-        while(j<=k){
-            
-			//if j is 0, lets swap with i'th index
-            if(nums[j]==0)
-            {
-                swapColors(nums,i,j);
-                i++;
-                j++;
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                mid++;
+                low++;
             }
-			// if we see 1, lets just move forward
-            else if(nums[j]==1)
-                j++;
-				
-			//if we see a 2, lets swap with k'th index and not moving j bcz we don't know what value j holds after swap
+            else if(nums[mid]==1){
+                mid++;
+            }
             else{
-                swapColors(nums,j,k);
-                k--;
+                swap(nums,high,mid);
+                high--;
             }
         }
     }
-    
-    public void swapColors(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
