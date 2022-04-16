6fd2882297db1f6fac5a30941ff048023a7bb191
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-04-16 18:21:13 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (17.74%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res=new ArrayList<List<Integer>>();
-        List<Integer> row,pre=null;
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> row,prev=null;
         for(int i=0;i<numRows;i++){
-            //creating new list
-            row=new ArrayList<Integer>();
+            row=new ArrayList<>();
             for(int j=0;j<=i;j++){
+                //if row has first and last column then 1 will bw added
                 if(j==0 || j==i){
                     row.add(1);
                 }else{
-                    row.add(pre.get(j-1)+pre.get(j));
+                    row.add(prev.get(j-1)+prev.get(j));
                 }
             }
-            pre=row;
+            prev=row;
             res.add(row);
         }
         return res;
