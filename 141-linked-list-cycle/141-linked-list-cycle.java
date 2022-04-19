f7ace2340cfe98b477a777bd7355d57ffe79906f
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-04-19 21:27:36 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (51.38%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,25 +11,19 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // ListNode slowptr=head;
-        // ListNode fastptr=head;
-        // while(fastptr!=null && fastptr.next!=null){
-        //     slowptr=slowptr.next;
-        //     fastptr=fastptr.next.next;
-        //     if(slowptr==fastptr){
-        //         return true;
-        //     }
-        // }
-        // return false;
-        ListNode slowptr=head;
-        ListNode fastptr=head;
-        while(fastptr!=null && fastptr.next!=null){
-            slowptr=slowptr.next;
-            fastptr=fastptr.next.next;
-            if(slowptr==fastptr){
-                return true;
-            }
+        if(head==null || head.next==null){
+            return false;
         }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+            return true;
+        }
+        }
+        
         return false;
     }
 }
