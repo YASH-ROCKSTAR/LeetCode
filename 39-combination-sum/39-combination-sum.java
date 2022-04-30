5945411eade6b1bb37526bba61504b7b9f4a695d
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-04-30 12:06:24 +0530

    Time: 4 ms (74.73%), Space: 44.5 MB (65.88%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,21 +1,26 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
-        sum(0,candidates,target,ans,new ArrayList<>());
+        solve(0,candidates,target,ans,new ArrayList<>());
         return ans;
     }
-    public void sum(int ind,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
-        if(ind==arr.length){
+    public void solve(int index,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
+        // when recursion tree reaches to last index
+        if(index==arr.length){
+            // when target get zero
             if(target==0){
                 ans.add(new ArrayList<>(ds));
             }
             return;
         }
-        if(arr[ind]<=target){
-            ds.add(arr[ind]);
-            sum(ind,arr,target-arr[ind],ans,ds);
+        //now checking if our index value is less than target then adding it to data structure
+        if(arr[index]<=target){
+            ds.add(arr[index]);
+            //when picking the element
+            solve(index,arr,target-arr[index],ans,ds);
             ds.remove(ds.size()-1);
         }
-        sum(ind+1,arr,target,ans,ds);
+        //when not pricking the element
+        solve(index+1,arr,target,ans,ds);
     }
 }
