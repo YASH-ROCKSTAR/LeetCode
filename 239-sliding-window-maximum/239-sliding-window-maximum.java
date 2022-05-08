87dbafaa1c4e8cd5eef178efc7757f34b7cac21c
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-05-08 08:44:47 +0530

    Time: 52 ms (56.37%), Space: 140.8 MB (42.18%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,25 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
-        //r is for storing result;
         int[] r=new int[n-k+1];
-        int ri=0; //for index at r
-        
-        //store index;
+        int ri=0;
         Deque<Integer> q=new ArrayDeque<>();
         for(int i=0;i<nums.length;i++){
-            // remove no out of range
+            //removing element out of bound
+            //mean if we are in second k size window and some element in deque 
+            //are present that doesnt belong to that window then we remove them
             if(!q.isEmpty() && q.peek()==i-k){
                 q.poll();
             }
-            //remve smaller number in k range as they are useless
-            //check from back
             while(!q.isEmpty() && nums[q.peekLast()]<=nums[i]){
                 q.pollLast();
             }
-            //put largest number
             q.offer(i);
-            //we take front which is max element from nums[q.peek()]
+            //putting greatest element of each k size window
             if(i>=k-1){
                 r[ri++]=nums[q.peek()];
             }
