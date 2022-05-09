bc51698f16960b5e6e8b5c494db1d19ade11ea22
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-05-10 00:29:53 +0530

    Added solution - LeetHub

---
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            int N = sc.nextInt();
+            int M[][] = new int[N][N];
+            for(int i=0; i<N; i++)
+            {
+                for(int j=0; j<N; j++)
+                {
+                    M[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println(new Solution().celebrity(M,N));
+            t--;
+        }
+    } 
+} // } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{ 
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(int M[][], int n)
+    {
+    	// code here 
+    	Stack<Integer> st=new Stack<>();
+    	for(int i=0;i<M.length;i++){
+    	    st.push(i);
+    	}
+    	while(st.size()>=2){
+    	    int i=st.pop();
+    	    int j=st.pop();
+    	   if(M[i][j]==1){
+    	           // mean that is kknow j --> means i is not a celebrity
+    	       st.push(j);
+    	   }else{
+    	           // i doesnt know j means--> j is not a celebrity
+    	       st.push(i);
+    	   }
+    	}
+    	int pot=st.pop();
+    // 	now check pot row and col
+        for(int i=0;i<n;i++){
+            if(i!=pot){
+                if(M[i][pot]==0 || M[pot][i]==1){
+                    return -1;
+                }
+            }
+           
+        } return pot;
+        
+    }
+}
