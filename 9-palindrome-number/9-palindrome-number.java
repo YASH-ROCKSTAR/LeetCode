624a8b8d9446ffb15bc7ce16cb5e3313c0d16a8f
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-05-11 00:42:09 +0530

    Time: 12 ms (65.21%), Space: 43.6 MB (71.85%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        if(temp<0){
+            return false;
+        }
+        int res=0;
+        while(temp>0){
+            int rem=temp%10;
+            res=res*10+rem;
+            temp=temp/10;
+        }
+        if(x==res){
+            return true;
+        }
+        return false;
+    }
+}
