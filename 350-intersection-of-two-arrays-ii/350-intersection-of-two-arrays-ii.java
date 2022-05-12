788cbe4dfe05ff0335d208c2bd1edc81f08f7462
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-05-13 01:49:08 +0530

    Time: 3 ms (77.29%), Space: 42.2 MB (95.18%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,36 +1,35 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> set=new HashMap<>();
         List<Integer> li=new ArrayList<>();
         HashMap<Integer,Integer> mp1=new HashMap<>();
         HashMap<Integer,Integer> mp2=new HashMap<>();
-        for(int i=0;i<nums1.length;i++){
-            if(mp1.containsKey(nums1[i])){
-                mp1.put(nums1[i],mp1.get(nums1[i])+1);
+        for(int i:nums1){
+            if(mp1.containsKey(i)){
+                mp1.put(i,mp1.get(i)+1);
             }else{
-                mp1.put(nums1[i],1);
+                mp1.put(i,1);
             }
         }
-        for(int i=0;i<nums2.length;i++){
-            if(mp2.containsKey(nums2[i])){
-                mp2.put(nums2[i],mp2.get(nums2[i])+1);
+        for(int i:nums2){
+            if(mp2.containsKey(i)){
+                mp2.put(i,mp2.get(i)+1);
             }else{
-                mp2.put(nums2[i],1);
+                mp2.put(i,1);
             }
-        }  
+        }
         for(Integer kk:mp1.keySet()){
             if(mp2.containsKey(kk)){
-                int x=Math.min(mp2.get(kk),mp1.get(kk));
-                while(x--> 0){
+                int x=Math.min(mp1.get(kk),mp2.get(kk));
+                while(x-->0){
                     li.add(kk);
                 }
             }
         }
         int[] arr=new int[li.size()];
-        for(int i=0;i<li.size();i++){
+        for(int i=0;i<arr.length;i++){
             arr[i]=li.get(i);
-            
         }
         return arr;
+
     }
 }
