31ac0f760550513ca3bbfa206bc4666a0247a02b
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-05-14 22:36:44 +0530

    Time: 1 ms (83.11%), Space: 43.8 MB (51.50%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,19 +1,19 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if(nums.length<=1 || nums==null) return;
-        int i=nums.length-2;
+        if(nums==null || nums.length<=1) return;
+        int i=nums.length-2; //split point can be at second last index
         while(i>=0 && nums[i]>=nums[i+1]) i--;
         if(i>=0){
             int j=nums.length-1;
-            while(nums[j]<=nums[i]) j--;
+            while(nums[j]<=nums[i]) j--;  //if it splits then comparing i with j finding next number greater than that
             swap(nums,i,j);
         }
         reverse(nums,i+1,nums.length-1);
     }
     public void swap(int[] nums,int i,int j){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
     public void reverse(int[] nums,int i,int j){
         while(i<j){
