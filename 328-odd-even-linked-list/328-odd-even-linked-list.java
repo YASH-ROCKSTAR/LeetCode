40922d01c4e3b8318e2a97c7b50c01b42f355d76
yashnigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-05-16 00:06:33 +0530

    Time: 1 ms (25.63%), Space: 44.4 MB (59.55%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.java
+++ 328-odd-even-linked-list/328-odd-even-linked-list.java
@@ -12,7 +12,6 @@ class Solution {
     public ListNode oddEvenList(ListNode head) {
         if(head!=null){
             ListNode odd=head;
-            //second elemnet will be even
             ListNode even=head.next;
             ListNode evenstart=even;
             while(odd.next!=null && even.next!=null){
@@ -21,12 +20,8 @@ class Solution {
                 even.next=odd.next;
                 even=even.next;
             }
-            //here last odd node will be appointing ro evenestart
             odd.next=evenstart;
-            //condition when list is off odd size then odd node will at null and even will be not at null
-            //so we have to make even null
-            
-        }  
+        }
         return head;
     }
 }
