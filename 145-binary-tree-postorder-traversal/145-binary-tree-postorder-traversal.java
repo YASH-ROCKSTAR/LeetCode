0b56b71a1c532bab0f5907bd9812bf8be12b3eca
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-05-21 19:09:28 +0530

    Time: 1 ms (43.69%), Space: 43 MB (5.21%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -15,15 +15,22 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> st1=new Stack<>();
+        Stack<TreeNode> st2=new Stack<>();
+        
         List<Integer> li=new ArrayList<>();
-        preorder(root,li);
-        return li;
-    }
-    public void preorder(TreeNode root,List<Integer> li){
-        if(root!=null){
-            preorder(root.left,li);
-            preorder(root.right,li);
-            li.add(root.val);
+        if(root==null) return li;
+        
+        st1.push(root);
+        while(!st1.isEmpty()){
+            root=st1.pop();
+            st2.add(root);
+            if(root.left!=null) st1.push(root.left);
+            if(root.right!=null) st1.push(root.right);
         }
+        while(!st2.isEmpty()){
+            li.add(st2.pop().val);
+        }
+        return li;
     }
 }
