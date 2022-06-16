994fdb13a858fbc82ad0e14b676772442d92b3e0
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-06-16 14:19:58 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (89.58%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,13 +15,13 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return root==null || symmetric(root.left,root.right);
+        return root==null || symm(root.left,root.right);
     }
-    public boolean symmetric(TreeNode left,TreeNode right){
+    public boolean symm(TreeNode left,TreeNode right){
         if(left==null || right==null){
             return left==right;
         }
         if(left.val!=right.val) return false;
-         return symmetric(left.left,right.right) && symmetric(left.right,right.left); 
+        return symm(left.left,right.right) && symm(left.right,right.left);
     }
 }
