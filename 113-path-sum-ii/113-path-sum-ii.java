6d191374ec6227800e3c44f5d5f25b95793551f5
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-06-17 16:48:40 +0530

    Time: 9 ms (10.69%), Space: 50.2 MB (12.72%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -14,64 +14,35 @@
  * }
  */
 class Solution {
-//     List<List<Integer>> ans;
-//     public void path(TreeNode root,int targetsum,List<Integer> l){
-//         if(root.left==null && root.right==null && targetsum-root.val==0){
-//             ans.add(new ArrayList<>(l));
-//         }
-//         List<Integer> list=new ArrayList<>(l);
-//         if(root.left!=null){
-//             list.add(root.left.val);
-//             path(root.left,targetsum-root.val,l);
-//             list.remove(list.size()-1);
-//         }
-//         if(root.right!=null){
-//             list.add(root.right.val);
-//             path(root.right,targetsum-root.val,l);
-//             list.remove(list.size()-1);
-            
-//         }
-//     }
-//     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-//         ans=new ArrayList<>();
-//         if(root==null){
-//             return ans;
-//         }
-//         List<Integer> l=new ArrayList<>();
-//         l.add(root.val);
-//         path(root,targetSum,l);
-//         return ans;
-        
-        
-//     }
     List<List<Integer>> ans;
-    private void solution(TreeNode root, int targetSum, List<Integer> list){
-        if(root.left == null && root.right == null && targetSum-root.val == 0){
+    public void path(TreeNode root,int targetsum,List<Integer> list){
+        if(root.left==null && root.right==null && targetsum-root.val==0){
             ans.add(new ArrayList<>(list));
         }
-        
-        List<Integer> l = new ArrayList<>(list);
-        if(root.left != null){
+        List<Integer> l=new ArrayList<>(list);
+        if(root.left!=null){
             l.add(root.left.val);
-            solution(root.left, targetSum-root.val, l);
+            path(root.left,targetsum-root.val,l);
             l.remove(l.size()-1);
         }
-        
-        if(root.right != null){
+        if(root.right!=null){
             l.add(root.right.val);
-            solution(root.right, targetSum-root.val, l);
+            path(root.right,targetsum-root.val,l);
             l.remove(l.size()-1);
+            
         }
     }
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        ans = new ArrayList<>();
-        if(root == null)
+        ans=new ArrayList<>();
+        if(root==null)
             return ans;
         
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
         list.add(root.val);
-        solution(root, targetSum, list);
+        path(root,targetSum,list);
         return ans;
+        
+        
     }
     
 }
