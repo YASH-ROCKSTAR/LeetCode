ee363544bf882bcd86b6e3cb332e0410046bceef
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-06-18 10:49:23 +0530

    Time: 6 ms (18.64%), Space: 44.4 MB (52.14%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,28 +15,28 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if(inorder==null || postorder==null || inorder.length!=postorder.length){
+        if(inorder==null || postorder==null ||inorder.length!=postorder.length){
             return null;
         }
-        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        Map<Integer,Integer> mp=new HashMap<Integer,Integer>();
         for(int i=0;i<inorder.length;i++){
-            map.put(inorder[i],i);
+            mp.put(inorder[i],i);
         }
+        TreeNode root=build(inorder,0,inorder.length-1,postorder,0,postorder.length-1,mp);
         
-        return build(inorder , 0 , inorder.length-1 , postorder , 0 , postorder.length-1 , map);
-        
+        return root;
     }
-    public TreeNode build(int[] inorder,int instart,int inend ,int[] postorder,int prestart,int preend,HashMap<Integer,Integer> map){
+    public TreeNode build(int[] inorder,int instart,int inend,int[] postorder,int poststart,int postend,Map<Integer,Integer> mp){
         
-        if(prestart>preend || instart>inend){
-            return null;
-        }
-        TreeNode root=new TreeNode(postorder[preend]);
-        int inroot=map.get(postorder[preend]);
-        int numleft=inroot-instart;
+        if(instart>inend || poststart>postend) return null;
         
-        root.left=build(inorder,instart,inroot-1,postorder,prestart,prestart+numleft-1,map);
-        root.right=build(inorder,inroot+1,inend,postorder,prestart+numleft,preend-1,map);
+        TreeNode root=new TreeNode(postorder[postend]);
+        int inroot=mp.get(root.val);
+       //no of element in left 
+        int left=inroot-instart;
+        
+        root.left=build(inorder,instart,inroot-1,postorder,poststart,poststart+left-1,mp);
+        root.right=build(inorder,inroot+1,inend,postorder,poststart+left,postend-1,mp);
         
         return root;
     }
