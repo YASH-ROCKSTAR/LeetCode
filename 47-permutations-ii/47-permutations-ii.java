f59ed0a82ac95bd4771f1c7034c175c06723bc49
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-06-28 17:27:56 +0530

    Time: 51 ms (20.62%), Space: 57.7 MB (6.68%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,32 +1,29 @@
 class Solution {
     HashSet<List<Integer>> set;
-     public void recurpermit(int index,int[] nums){
-       
-        if(index==nums.length){
-            List<Integer> ds=new ArrayList<>();
+    public void permu(int[] nums,int pos){
+        List<Integer> li=new ArrayList<>();
+        if(pos==nums.length){
             for(int i=0;i<nums.length;i++){
-                ds.add(nums[i]);
+                li.add(nums[i]);
             }
-            set.add(ds);
-            return ;
+            set.add(li);
         }
-        for(int i=index;i<nums.length;i++){
-            swap(i,index,nums);
-            //next element pe jaye ke swap karne ke liye
-            recurpermit(index+1,nums);
-            //backtracking 
-            swap(i,index,nums);
+        for(int i=pos;i<nums.length;i++){
+            swap(nums,pos,i);
+            permu(nums,pos+1);
+            swap(nums,pos,i);
+            
         }
     }
-    private void swap(int i,int j,int[] nums){
-        int t=nums[i];
-        nums[i]= nums[j];
-        nums[j]=t;
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
     public List<List<Integer>> permuteUnique(int[] nums) {
         set=new HashSet<>();
-        recurpermit(0,nums);
-        List<List<Integer>> ans=new ArrayList<>(set);
-        return ans;
+        permu(nums,0);
+        List<List<Integer>> list=new ArrayList<>(set);
+        return list;
     }
 }
