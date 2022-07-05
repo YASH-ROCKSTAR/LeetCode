68f72fdef40503808c790bfe8f8f9d76e65aad08
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-05 15:39:23 +0530

    Time: 5 ms (74.64%), Space: 43.4 MB (77.51%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -2,23 +2,21 @@ class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(candidates);
-        solve(0,candidates,target,ans,new ArrayList<>());
+        helper(0,candidates,target,new ArrayList<>(),ans);
         return ans;
     }
-    public void solve(int index,int[] arr,int target,List<List<Integer>> ans,List<Integer> ds){
-        // when target get zero
+    public void helper(int index,int[] arr,int target,List<Integer> li,List<List<Integer>> ans){
+        
         if(target==0){
-            ans.add(new ArrayList<>(ds));
-             return;
+            ans.add(new ArrayList<>(li));
+            return;
         }
-       
         for(int i=index;i<arr.length;i++){
             if(i>index && arr[i]==arr[i-1]) continue;
             if(arr[i]>target) break;
-            ds.add(arr[i]);
-            //when picking the element
-            solve(i+1,arr,target-arr[i],ans,ds);
-            ds.remove(ds.size()-1);
+            li.add(arr[i]);
+            helper(i+1,arr,target-arr[i],li,ans);
+            li.remove(li.size()-1);
         }
     }
 }
