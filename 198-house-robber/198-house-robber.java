dd64444da6428776b4295e09ad7cc90f9f27a7f5
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-07 19:00:32 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (54.73%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -6,7 +6,7 @@ class Solution {
         int prev=nums[0];
         int prev2=0;
         
-        for(int i=0;i<nums.length;i++){
+        for(int i=1;i<nums.length;i++){
             int take=nums[i];
             if(i>1){
                 take+=prev2;
