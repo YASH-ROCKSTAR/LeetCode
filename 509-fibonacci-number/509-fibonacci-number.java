9c937bd86526d05cf56cb0001f59a256e897a9b6
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-11 11:27:29 +0530

    Time: 0 ms (100.00%), Space: 41 MB (39.22%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,15 +1,14 @@
-class Solution {
-    public int solve(int n,int[] dp){
-        if(n<=1){
-            return n;
-        }
-        if(dp[n]!=-1) return dp[n];
-        
-        return dp[n]=fib(n-1)+fib(n-2);
-    }
+class Solution {   
     public int fib(int n) {
         int[] dp=new int[n+1]; 
-        Arrays.fill(dp,-1);
-        return solve(n,dp);
+        if(n==0){
+            return 0;
+        }
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
