fc4831cbd0278b92081c579fdfa561bf4ad195f2
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-11 13:23:50 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (49.53%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,15 +1,18 @@
 class Solution {
     public int solve(int n,int[] dp){
-        if(n==0 || n==1){
-            return 1;
+        // if(n==0){
+        //     return 0; 
+        // }
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-        
+        return dp[n];
     }
     public int climbStairs(int n) {
         int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
+        // Arrays.fill(dp,-1);
         return solve(n,dp);
     }
 }
