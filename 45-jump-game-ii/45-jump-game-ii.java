c28de268cde5f53c90c7367b8a648d7270bcb46e
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-12 21:08:37 +0530

    Time: 2 ms (80.88%), Space: 49.8 MB (25.26%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<nums.length-1;i++){
             curr=Math.max(curr,i+nums[i]);
             
-            if(i==max){
+            if(i>=max){
                 ans++;
                 max=curr;
             }
