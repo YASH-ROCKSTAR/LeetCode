a5cb84a54480cec45f2b192a4853e98f5d2fec4f
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-12 11:34:32 +0530

    Time: 3 ms (36.76%), Space: 54.9 MB (43.01%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -4,7 +4,7 @@ class Solution {
         int j=0;
         for(int i=0;i<n;i++){
             if(arr[i]!=0){
-               int temp=arr[i];
+                int temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
                 j++;
