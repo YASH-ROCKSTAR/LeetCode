3f67f093a8d5a0b7b688a07649b225965df4b049
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-15 14:22:51 +0530

    Time: 14 ms (43.58%), Space: 50.4 MB (48.05%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,26 +1,23 @@
 class Solution {
-    public int solve(int ind,int tar,int[] coins,int[][] dp){
-        if(ind==0){
-            if(tar%coins[0]==0){
-                return 1;
-            }else{
-                return 0;
-            }
-        }
-        if(dp[ind][tar]!=-1) return dp[ind][tar];
-        int take=0;
-        int not=solve(ind-1,tar,coins,dp);
-        if(coins[ind]<=tar){
-            take=solve(ind,tar-coins[ind],coins,dp);
-        }
-        return dp[ind][tar]=take+not;
-    }
+    
     public int change(int amount, int[] coins) {
         int n=coins.length;
         int[][] dp=new int[n][amount+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=1;
+            }
         }
-        return solve(n-1,amount,coins,dp);
+        for(int ind=1;ind<n;ind++){
+            for(int tar=0;tar<=amount;tar++){
+                int take=0;
+                int not=dp[ind-1][tar];
+                if(coins[ind]<=tar){
+                    take=dp[ind][tar-coins[ind]];
+                }
+                dp[ind][tar]=take+not;
+            }
+        }
+        return dp[n-1][amount];
     }
 }
