79ec68328dfef2031880cba0bfa75a5263ea3ba4
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-15 14:25:16 +0530

    Time: 6 ms (65.41%), Space: 41.8 MB (72.27%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -2,22 +2,25 @@ class Solution {
     
     public int change(int amount, int[] coins) {
         int n=coins.length;
-        int[][] dp=new int[n][amount+1];
+        // int[][] dp=new int[n][amount+1];
+        int[] prev=new int[amount+1];
+        int[] curr=new int[amount+1];
         for(int i=0;i<=amount;i++){
             if(i%coins[0]==0){
-                dp[0][i]=1;
+                prev[i]=1;
             }
         }
         for(int ind=1;ind<n;ind++){
             for(int tar=0;tar<=amount;tar++){
                 int take=0;
-                int not=dp[ind-1][tar];
+                int not=prev[tar];
                 if(coins[ind]<=tar){
-                    take=dp[ind][tar-coins[ind]];
+                    take=curr[tar-coins[ind]];
                 }
-                dp[ind][tar]=take+not;
+                curr[tar]=take+not;
             }
+            prev=curr;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 }
