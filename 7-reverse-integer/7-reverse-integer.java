9291757514559e698b1aaa32ebc38106823cdc6b
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-20 22:13:09 +0530

    Time: 2 ms (77.05%), Space: 41 MB (77.07%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        long res=0;
+        while(x!=0){
+            int rem=x%10;
+            res=res*10+rem;
+            x=x/10;
+        }
+        if(res<Integer.MIN_VALUE || res>Integer.MAX_VALUE)
+        {
+            return 0;
+        }
+        else{
+            return (int)res;
+        }
+    }
+}
