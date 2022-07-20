dfbb24dd02f938cfa8658157ae438bccbd14be72
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-20 22:33:04 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (51.68%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,18 +1,18 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        //binart search
-        int start=0;
-        int end=nums.length-1;
-        while(start<end){
-            int mid=start+(end-start)/2;
+        int s=0;
+        int e=nums.length-1;
+        while(s<e){
+            int mid=s+(e-s)/2;
             if(nums[mid]>nums[mid+1]){
-                end=mid;
-            }else{
+                e=mid;
+            }
+            else{
                 if(nums[mid]<nums[mid+1]){
-                    start=mid+1;
+                    s=mid+1;
                 }
             }
         }
-        return start;
+        return s;
     }
 }
