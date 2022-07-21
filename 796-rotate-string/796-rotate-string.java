9457d72148739731199dece47eb6d9a7975ae38f
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-21 23:28:54 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (74.11%) - LeetHub

--- 796-rotate-string/796-rotate-string.java
+++ 796-rotate-string/796-rotate-string.java
@@ -2,12 +2,14 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         if(s.length()!=goal.length()){
             return false;
+
         }
-        //s3 conatins all left shift of s
-        String s3=s+s;
-        if(s3.contains(goal)){
+        //as  said that left most charcter to be shift to last of string s
+        String str=s+s;
+        if(str.contains(goal)){
             return true;
         }
         return false;
+        
     }
 }
