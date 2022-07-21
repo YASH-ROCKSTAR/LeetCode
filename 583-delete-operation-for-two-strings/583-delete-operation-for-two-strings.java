ba46d8b6513ab1ef9e0b604286c73557683e51cd
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-21 14:41:56 +0530

    Time: 21 ms (22.29%), Space: 47.1 MB (43.28%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,16 +1,50 @@
 class Solution {
-   public int minDistance(String s1, String s2) {
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-        for (int i = 0; i <= s1.length(); i++) {
-            for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0 || j == 0)
-                    dp[i][j] = i + j;
-                else if (s1.charAt(i - 1) == s2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1];
-                else
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+    // public int solve(){
+    //     int m=s.length();
+    //     int n=t.length();
+    //     int[][] dp=new int[m+1][n+1];
+    //     for(int[] row:dp){
+    //         Arrays.fill(row,-1);
+    //     }
+    //     for(int i=0;i<=m;i++){
+    //         dp[i][0]=0;
+    //     }
+    //     for(int i=0;i<=n;i++){
+    //         dp[0][i]=0;
+    //     }
+    //     for(int i=1;i<=m;i++){
+    //         for(int j=1;j<n;j++){
+    //             if(s.charAt(i-1)==t.charAt(j-1)){
+    //                 dp[i][j]=1+dp[i-1][j-1];
+    //             }else{
+    //                 dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+    //             }
+    //         }
+    //     }
+    //     return dp[m][n];
+    // }
+    public int minDistance(String s,String t) {
+        int m=s.length();
+        int n=t.length();
+        int[][] dp=new int[m+1][n+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=n;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-        return dp[s1.length()][s2.length()];
+        return s.length()+t.length()-2*dp[s.length()][t.length()];
     }
 }
