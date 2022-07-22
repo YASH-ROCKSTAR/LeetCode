20cc095b8e29b3cdfdd2a001c6db7a1e4fed9919
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-22 21:44:07 +0530

    Time: 45 ms (13.16%), Space: 50.3 MB (45.69%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solve(int i,int j,String s,String t,int[][] dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
+        if(j==0) return 1;
+        if(i==0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)){
+        if(s.charAt(i-1)==t.charAt(j-1)){
             return dp[i][j]=solve(i-1,j-1,s,t,dp)+solve(i-1,j,s,t,dp);
         }
         return dp[i][j]=solve(i-1,j,s,t,dp);
@@ -16,6 +16,6 @@ class Solution {
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        return solve(m-1,n-1,s,t,dp);
+        return solve(m,n,s,t,dp);
     }
 }
