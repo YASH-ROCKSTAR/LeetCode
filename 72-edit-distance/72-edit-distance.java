dbc64e0091ec379f3b52c8fa636535f79cac2dbd
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-23 12:36:36 +0530

    Time: 9 ms (53.29%), Space: 44.8 MB (57.82%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,42 +1,27 @@
 class Solution {
     public int solve(String st,String tt,int i,int j,int[][] dp){
-        if(i<0) 
-            return j+1;
-        if(j<0) 
-            return i+1;
+        //now 1 based indexing
+        if(i==0) 
+            return j;
+        if(j==0) 
+            return i;
         
         if(dp[i][j]!=-1) return dp[i][j] ;
-        if(st.charAt(i)==tt.charAt(j))
+        if(st.charAt(i-1)==tt.charAt(j-1))
             return dp[i][j]=0+solve(st,tt,i-1,j-1,dp);
         
+        //miminum f three operation
         //delete ,insert and replace operation
         else return dp[i][j] = 1+Math.min(solve(st,tt,i-1,j-1,dp),
         Math.min(solve(st,tt,i-1,j,dp),solve(st,tt,i,j-1,dp)));
     }
-//     int solve(String S1, String S2, int i, int j, int[][] dp){
-    
-//         if(i<0)
-//             return j+1;
-//         if(j<0)
-//             return i+1;
-
-//         if(dp[i][j]!=-1) return dp[i][j];
-
-//         if(S1.charAt(i)==S2.charAt(j))
-//             return dp[i][j] =  0+solve(S1,S2,i-1,j-1,dp);
-
-//         // Minimum of three choices
-//         else return dp[i][j] = 1+Math.min(solve(S1,S2,i-1,j-1,dp),
-//         Math.min(solve(S1,S2,i-1,j,dp),solve(S1,S2,i,j-1,dp)));
-    
-//     }
     public int minDistance(String word1, String word2) {
         int m=word1.length();
         int n=word2.length();
-        int[][] dp=new int[m][n];
+        int[][] dp=new int[m+1][n+1];
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        return solve(word1,word2,m-1,n-1,dp);
+        return solve(word1,word2,m,n,dp);
     }
 }
