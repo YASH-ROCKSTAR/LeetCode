e445069110489f9c0cb79172974f893fafa783a4
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-30 16:12:18 +0530

    Time: 162 ms (13.47%), Space: 109.8 MB (10.83%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,22 +1,23 @@
 class Solution {
-    public int solve(int ind,int prev,int[] nums,int n,int[][] dp){
-        if(ind==n){
-            return 0;
-        }
-        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-        int len=0;
-        len=0+solve(ind+1,prev,nums,n,dp);
-        if(prev==-1 || nums[ind]>nums[prev]){
-            len=Math.max(len,1+solve(ind+1,ind,nums,n,dp));
-        }
-        return dp[ind][prev+1]=len;
-    }
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int[][] dp=new int[n][n+1];
+        int[][] dp=new int[n+1][n+1];
         for(int[] row:dp){
-            Arrays.fill(row,-1);
+            Arrays.fill(row,0);
         }
-        return solve(0,-1,nums,n,dp);
+        //no need to write base case as all array is fill with zero only
+        for(int ind=n-1;ind>=0;ind--){
+            // prev should be ind-1 because it should be always less than ind
+            //prev>=-1 because when it doesnot have it prev element
+            for(int prev=ind-1;prev>=-1;prev--){
+                int len=0+dp[ind+1][prev+1];
+                if(prev==-1 || nums[ind]>nums[prev]){
+                    len=Math.max(len,1+dp[ind+1][ind+1]);
+                } 
+                dp[ind][prev+1]=len;
+            }
+            
+        }
+        return dp[0][-1+1];
     }
 }
