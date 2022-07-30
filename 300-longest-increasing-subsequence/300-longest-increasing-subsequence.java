256fd30d3ca718d98ad7c679c5121418ae23dca5
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-07-30 16:16:05 +0530

    Time: 135 ms (14.98%), Space: 44.9 MB (23.52%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,23 +1,24 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        int[][] dp=new int[n+1][n+1];
-        for(int[] row:dp){
-            Arrays.fill(row,0);
-        }
+        int[] next=new int[n+1];
+        Arrays.fill(next,0);
+        int[] curr=new int[n+1];
+        Arrays.fill(curr,0);
         //no need to write base case as all array is fill with zero only
         for(int ind=n-1;ind>=0;ind--){
             // prev should be ind-1 because it should be always less than ind
             //prev>=-1 because when it doesnot have it prev element
             for(int prev=ind-1;prev>=-1;prev--){
-                int len=0+dp[ind+1][prev+1];
+                int len=0+next[prev+1];
                 if(prev==-1 || nums[ind]>nums[prev]){
-                    len=Math.max(len,1+dp[ind+1][ind+1]);
+                    len=Math.max(len,1+next[ind+1]);
                 } 
-                dp[ind][prev+1]=len;
+                curr[prev+1]=len;
             }
+            next=curr;
             
         }
-        return dp[0][-1+1];
+        return next[-1+1];
     }
 }
