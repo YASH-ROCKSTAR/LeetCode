53dc5753c61058ba3103d070a0787495ba2d45a5
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-01 20:06:32 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (19.17%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,39 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res=new ArrayList<>();
+        List<Integer> li=new ArrayList<>();
         if(matrix.length==0 || matrix[0].length==0){
-            return res;
+            return li;
         }
         int rowbeg=0;
         int rowend=matrix.length-1;
         int colbeg=0;
         int colend=matrix[0].length-1;
         while(rowbeg<=rowend && colbeg<=colend){
-            //trverse right
+            
+            //right
             for(int i=colbeg;i<=colend;i++){
-                res.add(matrix[rowbeg][i]);
+                li.add(matrix[rowbeg][i]);
             }
             rowbeg++;
-            
-            //traverse down
-            for(int j=rowbeg;j<=rowend;j++){
-                res.add(matrix[j][colend]);
+            //down
+            for(int i=rowbeg;i<=rowend;i++){
+                li.add(matrix[i][colend]);
             }
             colend--;
             
             if(rowbeg<=rowend){
                 for(int i=colend;i>=colbeg;i--){
-                    res.add(matrix[rowend][i]);
+                    li.add(matrix[rowend][i]);
                 }
                 rowend--;
             }
             if(colbeg<=colend){
-                for(int j=rowend;j>=rowbeg;j--){
-                    res.add(matrix[j][colbeg]);
+                for(int i=rowend;i>=rowbeg;i--){
+                    li.add(matrix[i][colbeg]);
                 }
                 colbeg++;
             }
         }
-        return res;     
+        return li;
+        
     }
 }
