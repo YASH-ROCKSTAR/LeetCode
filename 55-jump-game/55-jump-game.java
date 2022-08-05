a07390005c324424765cedea67af553e3e58cfd4
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-05 23:39:29 +0530

    Time: 2 ms (93.25%), Space: 68.5 MB (23.85%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,11 +1,9 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n=nums.length;
         int sum=0;
-        if(n==1) return true;
+        int n=nums.length;
         for(int i=0;i<nums.length && sum>=i;i++){
-            
-            if(sum<i+nums[i]){
+            if(sum<nums[i]+i){
                 sum=nums[i]+i;
             }
             if(sum>=n-1){
@@ -13,6 +11,5 @@ class Solution {
             }
         }
         return false;
-        
     }
 }
