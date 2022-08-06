e2b0515e63cff6b678929483df893b26869018d2
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-06 14:31:10 +0530

    Time: 7 ms (99.24%), Space: 47.6 MB (87.73%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         List<int[]> li=new ArrayList<>();
-        //boundary cndition
+        
         if(intervals.length==0 || intervals==null){
-            return li.toArray(new int[0][]);
+            return li.toArray(new int[0][1]);
         }
+        
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        
         int start=intervals[0][0];
         int end=intervals[0][1];
         for(int[] i:intervals){
             if(i[0]<=end){
-                end=Math.max(end,i[1]);
+                end=Math.max(i[1],end);
             }else{
                 li.add(new int[]{start,end});
                 start=i[0];
@@ -18,6 +20,6 @@ class Solution {
             }
         }
         li.add(new int[]{start,end});
-        return li.toArray(new int[0][]);
+        return li.toArray(new int[0][]);    
     }
 }
