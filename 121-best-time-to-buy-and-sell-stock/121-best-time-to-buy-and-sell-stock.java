763b5dba523d416333fbc49aa4b84a9e3058dc2c
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-07 16:08:15 +0530

    Time: 5 ms (13.16%), Space: 76.4 MB (80.22%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -3,10 +3,10 @@ class Solution {
         int n=prices.length;
         int mini=prices[0];
         int profit=0;
-        for(int i=1;i<n;i++){
+        for(int i=1;i<prices.length;i++){
             int cost=prices[i]-mini;
             profit=Math.max(profit,cost);
-            //as dp is remembering the past and so we are remembring the min;
+            
             mini=Math.min(mini,prices[i]);
         }
         return profit;
