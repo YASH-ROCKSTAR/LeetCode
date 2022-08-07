543bf74fd77041b4490f8b56c45bf9e5d8e9e901
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-07 14:13:23 +0530

    Time: 1 ms (91.31%), Space: 44.1 MB (15.39%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,19 +1,19 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if(nums==null || nums.length<=1) return;
-        int i=nums.length-2; //split point can be at second last index
-        while(i>=0 && nums[i]>=nums[i+1]) i--;
-        if(i>=0){
-            int j=nums.length-1;
-            while(nums[j]<=nums[i]) j--;  //if it splits then comparing i with j finding next number greater than that
+        int i=nums.length-2;
+        while(i>=0 && nums[i]>=nums[i+1]) i--;   //find first ith that break descending order
+        if(i>=0){               //if it is not entirely descending
+            int j=nums.length-1;    // starting from end
+            while(nums[j]<=nums[i]) j--;   //finding right most largest element
             swap(nums,i,j);
         }
         reverse(nums,i+1,nums.length-1);
+        
     }
-    public void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    public void swap(int[] A,int i,int j){
+        int temp=A[i];
+        A[i]=A[j];
+        A[j]=temp;
     }
     public void reverse(int[] nums,int i,int j){
         while(i<j){
