62dd7273acd74daa1a00fefd9e362e4761237fee
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-10 19:48:44 +0530

    Time: 2 ms (64.62%), Space: 44.6 MB (62.59%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -2,19 +2,15 @@ class Solution {
     public String restoreString(String s, int[] indices) {
         int n=s.length();
         
-        char ch[]= new char[n];
+        char[] ch=new char[n];
         
         for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
             
-            //iterating over string 
-            char sh=s.charAt(i);
-            //storing value of indices
             int val=indices[i];
             
-            //stoing the value ch with reshuffle
-            ch[val]=sh;
+            ch[val]=c;
         }
-        //reshuffle
         return String.valueOf(ch);
     }
 }
