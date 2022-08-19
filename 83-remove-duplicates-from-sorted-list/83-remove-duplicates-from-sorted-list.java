00610f70a5071382da49daf6afd6d5b9cfcfcb65
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-19 10:52:10 +0530

    Time: 1 ms (79.46%), Space: 43.6 MB (81.38%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.java
@@ -14,13 +14,14 @@ class Solution {
             return head;
         }
         ListNode curr=head;
-        while(curr.next!=null){
+        while(curr!=null && curr.next!=null){
             if(curr.val==curr.next.val){
                 curr.next=curr.next.next;
             }else{
                 curr=curr.next;
             }
         }
+        
         return head;
     }
 }
