a4476eaae6b5561e3bf4ab7b0bc07484fa67bc40
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-23 13:17:28 +0530

    Time: 2 ms (100.00%), Space: 57.2 MB (91.62%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -10,15 +10,23 @@
  */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
+        //take fast first and second on head
         ListNode fast=head,first=head,second=head;
+        //move fast to reach the beigging of k node 
+        //fast will reach to 2
         for(int i=0;i<k-1;i++){
             fast=fast.next;
         }
+        //allocate first to 2
         first=fast;
+        // run fast till its next is not null
         while(fast.next!=null){
+            //fast will reach at 2 and it next is null
+            //so second will 4 from last k last node(4)
             fast=fast.next;
             second=second.next;
         }
+        //then swappingg node in pair both 2 and 4 are swapped
         int temp=first.val;
         first.val=second.val;
         second.val=temp;
