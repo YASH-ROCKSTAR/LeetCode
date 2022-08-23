9374e9332d215a02d0fcdb023966dffb4e0b4e58
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-23 12:59:55 +0530

    Time: 5 ms (50.15%), Space: 42.4 MB (93.53%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.java
@@ -10,25 +10,33 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        //making a 2 stack and make fucntion to add number of l1 and l2 list into stack
         Stack<Integer> st1=addtostack(l1);
         Stack<Integer> st2=addtostack(l2);
         
-        
+        //take carry ==0
         int carry=0;
+        // storing ans into it
         ListNode temp=null;
+        //till both stack is empty and carry==1
         while(!st1.isEmpty() || !st2.isEmpty() ||carry==1){
             int sum=0;
+            //adding element
             if(!st1.isEmpty()){
                 sum+=st1.pop();
             }
             if(!st2.isEmpty()){
                 sum+=st2.pop();
             }
+            //if carry is ther then adding it into ans 
             sum+=carry;
-            carry=sum/10;
+            carry=sum/10;// sum is of two digit the  in carrry sum/10 will be stored last digit of dum
+            // storing sum into node to attach into it
             ListNode node=new ListNode(sum%10);
-            
+            //node .next is connected to temp
             node.next=temp;
+            //temp to node;
             temp=node;    
             
         }
