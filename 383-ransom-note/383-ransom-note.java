232166159d2a605f2a38d40244072869d4dd1068
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-25 22:18:38 +0530

    Time: 13 ms (53.55%), Space: 43.1 MB (86.25%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,5 +1,23 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        
+        //First take a hashmap and question says that 
+        //ransom note can be constructed by using magazine
+        //so the first convert bothe the string to chararray and 
+        //used for loop to iterate
+        // put the count of each variable of magazine in hashmap
+        //and the making rasomnote charaaray and 
+        //get count of each element from map it count for an elemt goes less than 0
+        //then it will retrun false //if it is 0 or greater than zero than it can mnake 
+        //rasom strin gfrm magazine
+        //taking dirst example
+        // ransomNote = "aa", magazine = "aab"
+        //putting magazine into hasmap
+        //and a--count will be 2 and b count wil be 1
+        // now running for loop for ransom note and getting the element from hasmap
+        //in first iteration uit get count of a 1 as it geti a and 2-1 it lwft count 1 
+        // again get a from map 1-1 =0 and puuting elent intop it zero so
+        //it is true
         HashMap<Character,Integer> maga=new HashMap<>();
         for(char c:magazine.toCharArray()){
             int count=maga.getOrDefault(c,0)+1;
