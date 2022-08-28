0457bb07675f9f0e3f9967dd31c8759a935d7e6c
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-29 01:08:04 +0530

    Time: 1 ms (98.35%), Space: 42.2 MB (98.38%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,34 +1,28 @@
 class Solution {
-    public void func(int[] nums,List<Integer> ds,List<List<Integer>> li,boolean[] freq){
-        //so if nums length == list size so add that list in list of list
-        if(nums.length==ds.size()){
-            li.add(new ArrayList<>(ds));
-            return;
-        }
-        //run a for loop in array and if
-        // freq array is not visisted 
-        for(int i=0;i<nums.length;i++){
-            
-            if(!freq[i]){
-                //then marked it as true
-                freq[i]=true;
-                //add in list
+    public void func(int pos,int[] nums,List<List<Integer>> li){
+        List<Integer> ds =new ArrayList<>();
+        if(pos==nums.length){
+            for(int i=0;i<nums.length;i++){
                 ds.add(nums[i]);
-                // call the function
-                func(nums,ds,li,freq);
-                //and removing that element when we go abck after the end call 
-                ds.remove(ds.size()-1);
-                //removing the element from list
-                //also marking not visited in freq array
-                freq[i]=false;
             }
+            li.add(new ArrayList<>(ds));
+            // return;
         }
+        for(int i=pos;i<nums.length;i++){
+            swap(nums,pos,i);
+            func(pos+1,nums,li);
+            swap(nums,pos,i);
+        }
+    }
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> li=new ArrayList<>();
         List<Integer> ds=new ArrayList<>();
-        boolean[] freq=new boolean[nums.length];
-        func(nums,ds,li,freq);
+        func(0,nums,li);
         return li;
     }
 }
