82e77b0fddc02699b9eabf941235bc031a48eed2
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-28 23:45:04 +0530

    Time: 2 ms (76.82%), Space: 45.1 MB (19.35%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,26 +1,34 @@
 class Solution {
+    public void func(int[] nums,List<Integer> ds,List<List<Integer>> li,boolean[] freq){
+        //so if nums length == list size so add that list in list of list
+        if(nums.length==ds.size()){
+            li.add(new ArrayList<>(ds));
+            return;
+        }
+        //run a for loop in array and if
+        // freq array is not visisted 
+        for(int i=0;i<nums.length;i++){
+            
+            if(!freq[i]){
+                //then marked it as true
+                freq[i]=true;
+                //add in list
+                ds.add(nums[i]);
+                // call the function
+                func(nums,ds,li,freq);
+                //and removing that element when we go abck after the end call 
+                ds.remove(ds.size()-1);
+                //removing the element from list
+                //also marking not visited in freq array
+                freq[i]=false;
+            }
+        }
+    }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> li=new ArrayList<>();
-        solve(nums,0,li);
+        List<Integer> ds=new ArrayList<>();
+        boolean[] freq=new boolean[nums.length];
+        func(nums,ds,li,freq);
         return li;
-    }
-    public void solve(int[] nums,int pos,List<List<Integer>> li){
-        List<Integer> ans=new ArrayList<>();
-        if(pos==nums.length){
-            for(int i=0;i<nums.length;i++){
-                ans.add(nums[i]);
-            }
-            li.add(ans);
-        }
-        for(int i=pos;i<nums.length;i++){
-            swap(nums,pos,i);
-            solve(nums,pos+1,li);
-            swap(nums,pos,i);
-        }
-    }
-    public void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
     }
 }
