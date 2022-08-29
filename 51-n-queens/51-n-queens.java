412c081a7dcac660947880ff2566ea4d46aa91cf
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-29 17:09:37 +0530

    Time: 11 ms (29.44%), Space: 47.4 MB (10.62%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,67 +1,74 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        boolean[][] board =new boolean[n][n];
+        boolean[][] board=new boolean[n][n];
         return Queen(board,0);
     }
-    public List<List<String>> Queen(boolean[][] board,int row){
-        if(row==board.length){
-            return display(board);
-        }
-        List<List<String>> arr=new ArrayList<>();
-        for(int col=0;col<board[0].length;col++){
-            if(isSafe(board,row,col)){
-                board[row][col]=true;
-                arr.addAll(Queen(board,row+1));
-                board[row][col]=false;
-            }
-        }
-        return arr;
-    }
-    public static boolean isSafe(boolean[][] board, int row, int col) {
-        //vertical row
+    public boolean isSafe(boolean[][] board,int row,int col){
+        
+        //reason for not checking on right side of queen as we are not able to reaches there beacuse we are moving from backward whatever queen attack will be there it will be on left side
+        
+        //on upper diagonal bothe col and row will decrease 
+        // upward diagonal on left side 
         for(int i=0;i<row;i++){
-            //cheking if queen is there then it is not safe
             if(board[i][col]){
                 return false;
             }
         }
-        //diagonal left
         int maxleft=Math.min(row,col);
         for(int i=1;i<=maxleft;i++){
-            //if true
             if(board[row-i][col-i]){
                 return false;
             }
         }
-        //diagonal right
-        int maxRight=Math.min(row, board.length-col-1);
-        for(int i=1;i<=maxRight;i++){
-            //if true
+        //lower diagonal which is on left side
+        int maxright=Math.min(row,board.length-col-1);
+        for(int i=1;i<=maxright;i++){
             if(board[row-i][col+i]){
                 return false;
             }
         }
-        //if none of checks are matched then true
         return true;
-    }
-
-    public List<List<String>> display(boolean[][] board) {
-        List<List<String>> list = new ArrayList<>();
-        List<String> sublist = new ArrayList<>();
         
-        for (int r = 0; r < board.length; r++) {
-            String x = "";
-            for (int c = 0; c < board.length; c++) {
-                if  (board[r][c]) {
-                    x += "Q";
-                } else {
-                    x += ".";
+    }
+    public List<List<String>> Queen(boolean[][] board,int row){
+        //if row size reaches to board size then
+        // add the queen to matrix
+        if(row==board.length){
+            return display(board);
+        }
+        
+        List<List<String>> li=new ArrayList<>();
+        //running a for loop for every column and row checking whether it is safe 
+        //safe a function which tell if queen is not getting attacked by another queen in upper diagional ,left col ,and lower diagonal
+        for(int col=0;col<board.length;col++){
+            if(isSafe(board,row,col)){
+                //is in a matrix if q ueen is safe it marked as true
+                board[row][col]=true;
+                //now calling for another row so row+1 in another list
+                li.addAll(Queen(board,row+1));
+                //and marking present row as false to backtract as wwe are now going to nextrow
+                board[row][col]=false;
+                
+            }
+            
+        }
+        return li;
+    }
+    public List<List<String>> display(boolean[][] board){
+        List<List<String>> li=new ArrayList<>();
+        List<String>arr=new ArrayList<>();
+        for(int r=0;r<board.length;r++){
+            String x="";
+            for(int c=0;c<board.length;c++){
+                if(board[r][c]){
+                    x+="Q";
+                }else{
+                    x+=".";
                 }
             }
-            sublist.add(x);
+            arr.add(x);
         }
-        list.add(sublist);
-        return list;
+        li.add(arr);
+        return li;
     }
-    
 }
