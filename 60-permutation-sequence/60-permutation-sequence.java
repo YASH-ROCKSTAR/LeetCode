df30eb378f3658df6787c02c27d985edd5674009
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-30 22:20:02 +0530

    Time: 2 ms (92.25%), Space: 39.9 MB (93.86%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.java
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -6,27 +6,19 @@ class Solution {
             fact=fact*i;
             ans.add(i);
         }
-        //adding nth element
         ans.add(n);
-        //taking string for storing ans;
         String arr="";
-        //we are using 0th based indexing so
         k=k-1;
         while(true){
-            //getting k/fact index no into ans string
             arr=arr+ans.get(k/fact);
-                //removing that number from array to decrease size
             ans.remove(k/fact);
-            //if array is empty then
             if(ans.size()==0){
                 break;
             }
-            //getting next value from here fro string
+            
             k=k%fact;
-            //getting new fact value;
             fact=fact/ans.size();
         }
         return arr;
-        
     }
 }
