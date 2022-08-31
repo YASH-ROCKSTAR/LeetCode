3407f32e7d40e71cc00c69ca0e681a227f008166
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-08-31 23:40:15 +0530

    Time: 12 ms (15.12%), Space: 43.9 MB (24.26%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,4 +1,8 @@
 class Solution {
+    //very simple nad a crisp solution is there
+    //using stack
+    //so process will be that we will be we will be pushing index of bracket and with that we will calculating longest valid bracket so firstly we will be pushing index value of open bracket if we are getting else if it is done then pop the element from stack and checking if stack is empty 
+    //then put that index value and else calculate max value so at last index is at two and stack has {-1,0} left sol res is (0,2-0) so max value is 2 
     public int longestValidParentheses(String s) {
         Stack<Integer> st=new Stack<>();
         int res=0;
