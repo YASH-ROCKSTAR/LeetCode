c36e00f724f0e349368a361276b111d7bd4609e4
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-09-02 19:21:52 +0530

    Time: 7 ms (37.84%), Space: 43.3 MB (25.14%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -1,31 +1,30 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        int[] alpha = new int[26];
-        HashMap<Character, Integer> map =  new HashMap<>();
-        Stack<Character> stack = new Stack<>();
+        int[] arr=new int[26];
         
-        //Store the last Index of the character.
-        for(int i = 0; i < s.length(); i++) map.put(s.charAt(i), i);
+        HashMap<Character,Integer> mp=new HashMap<>();
+        Stack<Character> st=new Stack<>();
         
-		//TRAVERSE EACH CHARACTER
-        for(int i = 0; i < s.length(); i++){
-            char curr = s.charAt(i);
-			
-			//IF ALREADY ADDED, CONTINUE
-            if(alpha[curr - 'a'] == 1) continue;
-			
-			//IF WE CAN POP THE PEEK BECAUSE IN NEXT TRAVERSAL I CAN ADD THIS CHARACTER 
-			//BY USING IT'S LAST INDEX.
-            while(!stack.isEmpty() && curr < stack.peek() && i < map.get(stack.peek())){
-			// ONCE I POP I HAVE TO CHANGE THE ALPHA ARRAY TOO.
-                alpha[stack.pop() - 'a'] = 0;
-            }
-            stack.push(curr);
-            alpha[curr - 'a'] = 1;
+        for(int i=0;i<s.length();i++){
+            mp.put(s.charAt(i),i);
         }
-        
-        StringBuilder sb = new StringBuilder();   
-        while(!stack.isEmpty()) sb.append(stack.pop());
+        for(int i=0;i<s.length();i++){
+            char curr=s.charAt(i);
+            
+            if(arr[curr-'a']==1){
+                continue;
+            }
+            
+            while(!st.isEmpty() && st.peek()>curr && mp.get(st.peek())>i){
+                arr[st.pop()-'a']=0;
+            }
+            st.push(curr);
+            arr[curr-'a']=1;
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
         return sb.reverse().toString();
     }
 }
