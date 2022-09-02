0b5ede37252f89b31fbfd82a2e0edb029b773b8a
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-09-02 16:55:28 +0530

    Time: 29 ms (73.76%), Space: 43.6 MB (90.17%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.java
+++ 402-remove-k-digits/402-remove-k-digits.java
@@ -3,6 +3,11 @@ class Solution {
         if(nums.length()==k){
             return "0";
         }
+        //like we take example "1432219" and k=3
+        //so first of fall we will be pushing eleemnt in stack and also checking that 
+        //in mean while whatever element is in last in stack peeking it out and and checking it with cuurent element that whether it is greater than it then pop it out it will happen till k>0
+        //so first 1 is pushed then 4 as it goes to i=2 then so it peek out 4 and it greater than 3 (4>3) so pop it out and add 3 in stack then it goes to i=3 element 2 and check while loop that(3>2) so pop it out add 2 and again add 2 and then it goes to 1 then check that 2>1 and pop it out now now add 9 
+        //into it so now stack contian 9121 so it it in reverse order storing element in stringbuilder then reverse it and also delete leading 0 it ther in string then return with toString method
         Stack<Character> st=new Stack<>();
         int i=0;
         while(i<nums.length()){
@@ -12,8 +17,6 @@ class Solution {
             }
             st.push(nums.charAt(i));
             i++;
-            
-            // corner case when all digits are equal
         }
         while(k>0){
             st.pop();
@@ -26,9 +29,7 @@ class Solution {
         sb.reverse();
         while(sb.length()>1 && sb.charAt(0)=='0'){
             sb.deleteCharAt(0);
-        } 
+        }
         return sb.toString();
-        
-                                                    
     }
 }
