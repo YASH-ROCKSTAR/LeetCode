900ccf4fabc9a4d849b0443c9121eb613afecc80
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-09-04 13:32:14 +0530

    Time: 50 ms (38.45%), Space: 111.1 MB (32.29%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.java
@@ -1,5 +1,11 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
+        //so question says that if it has subaaray sum that is always a multiple of k
+        //otherwise false
+        //so we take hashmap to store value of sum at ith position
+        //so we add sum to the and check if is multipleof k by dividing by k 
+        //if sum ==0 and check i>0 then it is true 
+        //another condition is if we contains sum in map and ig i-mp.get(sum)->value >1(checking the length is greater than 1 as it is said atleast size should be two) so it is true
         HashMap<Integer,Integer>mp=new HashMap<>();
         int sum=0;
         for(int i=0;i<nums.length;i++){
