c7e69ec88984646d8f86144a8276744b2aa87035
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-09-04 19:28:09 +0530

    Time: 35 ms (88.41%), Space: 61.4 MB (88.49%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -5,9 +5,6 @@ class Solution {
         int ri=0;
         Deque<Integer> q=new ArrayDeque<>();
         for(int i=0;i<nums.length;i++){
-            //removing element out of bound
-            //mean if we are in second k size window and some element in deque 
-            //are present that doesnt belong to that window then we remove them
             if(!q.isEmpty() && q.peek()==i-k){
                 q.poll();
             }
@@ -15,7 +12,6 @@ class Solution {
                 q.pollLast();
             }
             q.offer(i);
-            //putting greatest element of each k size window
             if(i>=k-1){
                 r[ri++]=nums[q.peek()];
             }
