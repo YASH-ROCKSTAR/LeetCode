d8264d986cede0c8981f529a0003a72c8516480d
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-09-28 00:23:11 +0530

    Time: 4 ms (65.73%), Space: 84.1 MB (57.82%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -3,24 +3,24 @@ class Solution {
         if(nums.length==1){
             return 1;
         }
-        int maxi=Integer.MIN_VALUE;
-        int mini=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
         int minInd=0;
         int maxInd=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]>maxi){
-                maxi=nums[i];
+            if(nums[i]>max){
+                max=nums[i];
                 maxInd=i;
             }
-            if(nums[i]<mini){
-                mini=nums[i];
+            if(nums[i]<min){
+                min=nums[i];
                 minInd=i;
             }
         }
-        int frontdelete=Math.max(minInd,maxInd)+1;
-        int lastdelete=nums.length-Math.min(minInd,maxInd);
-        int deletefrombothside=Math.min(minInd,maxInd) + nums.length - Math.max(minInd,maxInd) + 1;
+        int deletefront=Math.max(minInd,maxInd)+1;
+        int deletelast=nums.length-Math.min(minInd,maxInd);
+        int deletefrombothside=Math.min(minInd,maxInd)+nums.length-Math.max(minInd,maxInd)+1;
         
-        return Math.min(frontdelete,Math.min(lastdelete,deletefrombothside));    
+        return Math.min(deletefront,Math.min(deletelast,deletefrombothside));
     }
 }
