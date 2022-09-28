908c9ff8ac2256ac6ce642811cadc77dd170c50a
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-09-28 21:03:31 +0530

    Time: 1 ms (11.33%), Space: 42.7 MB (54.09%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,6 +1,5 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //traversing
         for(int i=0;i<matrix.length;i++){
             for(int j=i;j<matrix[0].length;j++){
                 int temp=matrix[i][j];
@@ -8,7 +7,6 @@ class Solution {
                 matrix[j][i]=temp;
             }
         }
-        //then reversing each row 
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix.length/2;j++){
                 int temp=matrix[i][j];
