1320d9c1e4d4fccc57304cea4af67f2bb29916bc
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-10-10 14:57:04 +0530

    Time: 21 ms (10.13%), Space: 41.2 MB (24.23%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,19 +1,17 @@
-class Solution {   
+class Solution {
     public int fib(int n) {
-        //so base condition is what if n==0 to return return 0
-       if(n==0){
-           return 0;
-       }
-        //if n==1 then return 1 as prev 0 +1=1
-        // so instead of doing that we have made base case and also 
-        //reason for doing so is that as we are taking last two element to do retrun current elemnt in series 
+        return solve(n);
+    }
+    public int solve(int n){
+        
+        if(n==0){
+            return 0;
+        }
         if(n==1){
             return 1;
         }
-        //this is recursive function we need to made
-        int ans=fib(n-1)+fib(n-2);
-        
-        //finally return ans;
-        return ans;
+        int a=solve(n-1);
+        int b=solve(n-2);
+        return a+b;
     }
 }
