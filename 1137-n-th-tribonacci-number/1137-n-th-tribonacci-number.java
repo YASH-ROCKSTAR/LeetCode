b033b9f4a48e6e7ae5bdd30252b867b86d31df83
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-10-12 00:37:54 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (89.53%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,18 +1,25 @@
 class Solution {
-    public int solve(int n,int[] dp){
-        if(n<2){
-            return n;
+    public int tribonacci(int n) {
+        return solve(n,new HashMap<>());
+    }
+    public int solve(int n,HashMap<Integer,Integer> mp){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
         }
         if(n==2){
             return 1;
         }
-        if(dp[n]!=-1) return dp[n];
-        
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
-    }
-    public int tribonacci(int n) {
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n,dp);
+        int current=n;
+        if(mp.containsKey(current)){
+            return mp.get(current);
+        }
+        int a=solve(n-1,mp);
+        int b=solve(n-2,mp);
+        int c=solve(n-3,mp);
+        mp.put(current,a+b+c);
+        return a+b+c;
     }
 }
