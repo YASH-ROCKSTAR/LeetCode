270443cd088d0511f8ffdcc7a18f16b83e5ea805
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-10-12 23:24:32 +0530

    Time: 253 ms (6.64%), Space: 42.2 MB (5.35%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,21 @@
 class Solution {
-    public int fib(int n) {
-        return solve(n);
+    public int fib(int n){
+        return solve(n,new HashMap<Integer,Integer>());
     }
-    public int solve(int n){
+    public int solve(int n,HashMap<Integer,Integer> mp){
         if(n==0){
             return 0;
         }
         if(n==1){
             return 1;
         }
-        int a=solve(n-1);
-        int b=solve(n-2);
-        
+        int current=n;
+        if(mp.containsKey(current)){
+            mp.get(current);
+        }
+        int a=solve(n-1,mp);
+        int b=solve(n-2,mp);
+        mp.put(current,a+b);
         return a+b;
         
     }
