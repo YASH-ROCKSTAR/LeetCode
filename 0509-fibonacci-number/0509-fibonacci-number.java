00e1b60642411d3f52e2644b7b81aff93359ce01
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-10-12 23:22:05 +0530

    Time: 20 ms (13.30%), Space: 40.7 MB (55.39%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        return solve(n);
+    }
+    public int solve(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        int a=solve(n-1);
+        int b=solve(n-2);
+        
+        return a+b;
+        
+    }
+}
