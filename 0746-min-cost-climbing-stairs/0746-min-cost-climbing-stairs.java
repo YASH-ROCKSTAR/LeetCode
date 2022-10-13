cb67be665aa0446e2f83d87de88f16c1bd56cea9
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-10-13 09:58:11 +0530

    Time: 8 ms (5.35%), Space: 46.5 MB (5.20%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,8 +1,7 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         HashMap<Integer,Integer> mp=new HashMap<Integer,Integer>();
-        //so here we have solve problrm for 0 index in that we also calculated for 1 also so we can get answer from there
-        return Math.min(solve(cost,0,mp),mp.get(1));
+        return Math.min(solve(cost,0,mp),mp.get(1));   
     }
     public int solve(int[] cost,int current,HashMap<Integer,Integer> mp){
         if(current==cost.length){
@@ -11,14 +10,13 @@ class Solution {
         if(current>cost.length){
             return 1001;
         }
-        int currentkey=current;
-        if(mp.containsKey(currentkey)){
-            return mp.get(currentkey);
+        int curr=current;
+        if(mp.containsKey(curr)){
+            return mp.get(curr);
         }
-        int onejump=cost[current]+solve(cost,current+1,mp);
-        int twojump=cost[current]+solve(cost,current+2,mp);
-       
-        mp.put(currentkey,Math.min(onejump,twojump));
-        return mp.get(currentkey);
+        int one=cost[current]+solve(cost,current+1,mp);
+        int two=cost[current]+solve(cost,current+2,mp);
+        mp.put(curr,Math.min(one,two));
+        return mp.get(curr);
     }
 }
