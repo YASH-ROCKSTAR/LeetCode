4b1391e798291e38bfd331aa4051231cff813212
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-10-14 22:47:32 +0530

    Time: 509 ms (36.78%), Space: 118.7 MB (5.00%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,17 +1,21 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(nums,0,target);
+        return solve(nums,0,target,new HashMap<String,Integer>());
     }
-    public int solve(int[] nums,int current,int target){
+    public int solve(int[] nums,int current,int target,HashMap<String,Integer> mp){
         if(current==nums.length && target==0){
             return 1;
         }
         if(current==nums.length && target!=0){
             return 0;
         }
-        int pos=solve(nums,current+1,target-nums[current]);
-        int neg=solve(nums,current+1,target+nums[current]);
-        
-        return pos+neg;
+        String currentkey=Integer.toString(current) + "-" +Integer.toString(target);
+        if(mp.containsKey(currentkey)){
+            return mp.get(currentkey);
+        }
+        int pos=solve(nums,current+1,target-nums[current],mp);
+        int neg=solve(nums,current+1,target+nums[current],mp);
+        mp.put(currentkey,pos+neg);
+        return mp.get(currentkey);
     }
 }
