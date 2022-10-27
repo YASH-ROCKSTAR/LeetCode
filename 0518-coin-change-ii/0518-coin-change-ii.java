24ea2a2808c5722f54b4827d9ed04837e4305233
Yash Nigam <67096014+YASH-ROCKSTAR@users.noreply.github.com>
2022-10-28 00:53:21 +0530

    Time: 34 ms (15.24%), Space: 50.8 MB (33.99%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,26 +1,32 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        return solve(coins,0,amount,new HashMap<String,Integer>());
+        int n=coins.length;
+        int[][] dp=new int[n][amount+1];
+        for(int[] i:dp){
+            Arrays.fill(i,-1);
+        }
+        return solve(coins,0,amount,dp);
     }
-    public int solve(int[] nums,int current,int amount,HashMap<String,Integer> mp){
+    public int solve(int[] nums,int current,int amount,int[][] dp){
         if(amount==0){
             return 1;
         }
         if(current>=nums.length){
             return 0;
         }
-        String currentkey=Integer.toString(current)+"-"+Integer.toString(amount);
-        if(mp.containsKey(currentkey)){
-            return mp.get(currentkey);
+        if(dp[current][amount]!=-1){
+            return dp[current][amount];
         }
+        
         int curcoin=nums[current];
         int take=0;
         if(curcoin<=amount){
-            take=solve(nums,current,amount-curcoin,mp);
+            take=solve(nums,current,amount-curcoin,dp);
         }
-        int nottake=solve(nums,current+1,amount,mp);
-        mp.put(currentkey,take+nottake);
-        return mp.get(currentkey);
+        int nottake=solve(nums,current+1,amount,dp);
+        
+        dp[current][amount]=take+nottake;
+        return dp[current][amount];
         
     }
 }
